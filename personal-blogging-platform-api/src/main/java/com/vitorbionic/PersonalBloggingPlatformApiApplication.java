a67e2d52187081c5df362c5f50b2601ea2e5cc0d
Vitor Bionic <vitorbionic@hotmail.com>
2025-02-12 15:55:30 -0300

    Setting the spring project

---
+++ personal-blogging-platform-api/src/main/java/com/vitorbionic/PersonalBloggingPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.vitorbionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingPlatformApiApplication.class, args);
+	}
+
+}
